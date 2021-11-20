420745afc7eb488fd0b6589bc8f6d67a457360d3
Suri Feng <0924suri@gmail.com>
2021-11-19 22:54:02 -0800

    Mask partly done

--- mask.py
+++ mask.py
@@ -1,27 +1,64 @@
 from PIL import Image
 import numpy as np
-# Open the image form working directory
-image = Image.open('SegmentationClass/frame_000000.png')
+import matplotlib.pyplot as plt
+import read_excel
+
+
+class area:
+    # TODO
+    def __init__(self, df_excel, clip_name):
+        self.c = read_excel.clip_info(df, clip_name)
+        self.mask_folder_path = "clip_name"  ####
+        self.start = self.c.zero_frame
+        self.end = self.c.lastframe
+        self.T = self.end - self.start + 1
+        self.mask_frame_path = np.zeros(self.T)
+        self.masks = self.get_masks()  # T*W*H
+        self.all_frame_area = self.get_all_frame_area()  # T*1
+
+    # TODO
+    def get_masks(self):
+        image = Image.open(self.mask_frame_path)
+        # image = Image.open(
+        #     "data/task_a01_5ml-l0-5-2021_06_04_08_51_29-segmentation mask 1.1/SegmentationObject/frame_000000.png"
+        # )
+        data = np.asarray(image)
+        data_gray = 1 / 3 * (data[:, :, 0] + data[:, :, 1] + data[:, :, 2])
+
+    # DONE 
+    def get_all_frame_area(self):
+        return np.count_nonzero(np.reshape(self.masks, (self.T, np.product(self.masks.shape))), axis=1)
+
+    # DONE
+    def frame_area(self, frame_num):
+        return self.all_frame_area[frame_num - self.start]
+
+    # DONE 
+    def percentage(self, frame_s, frame_l):
+        return (self.all_frame_area[frame_l - self.start] - self.all_frame_area[frame_s - self.start]) / self.all_frame_area[frame_l - self.start]
+
+    # TODO 
+    def find_min_or_max_extreme_area(self):
+        pass
+
+
+if __name__ == "__main__":
+    excel_name = "A.xlsx"
+    r_e = read_excel.r_excel(excel_name)
+    df = r_e.df_excel
+
+    clip_name = "a01_5ml-l0-5"
+    r_c = read_excel.clip_info(df, clip_name)
+    df_clip = r_c.df_clip
+    gh_onset = int(df_clip["GHR"].iloc[0])
+    gh_maximum = int(df_clip["GHM"].iloc[0])
+
+# max_value = np.max(data)
 # summarize some details about the image
-print(image.format)
-print(image.size)
-print(image.mode)
-
-np.set_printoptions(threshold=np.inf)
-# convert image to numpy array
-data = np.asarray(image)
-print(type(data))
-# summarize shape
-print(data.shape)
-
-# # load and display an image with Matplotlib
-# from matplotlib import image
-# from matplotlib import pyplot
-# # load image as pixel array
-# image = image.imread('SegmentationClass/frame_000000.png')
-# # summarize shape of the pixel array
-# print(image.dtype)
-# print(image.shape)
-# # display the array of pixels as an image
-# pyplot.imshow(image)
-# pyplot.show()
+# print(image.format)
+# print(image.size)
+# print(image.mode)
+# plt.imshow(data[:, :, 0].astype(np.uint8), cmap=plt.cm.gray)
+# plt.show()
+# print(type(data))
+# print(data.shape)
--- read_excel.py
+++ read_excel.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 class r_excel:
     def __init__(self, excel_name):
         self.excel_file = "data/" + excel_name
@@ -17,32 +16,33 @@ class clip_info:
     def __init__(self, df_excel, clip_name):
         self.clip_file = clip_name
         self.df_clip = df_excel[df_excel["Video Name"].str.lower() == clip_name]
-        self.zero_frame = int(self.df_clip["SS"].iloc[0])
+        self.zero_frame = int(self.df_clip["SS"].iloc[0]) 
+        self.last_frame = int(self.df_clip["SE"].iloc[0])
+        # most of final frames are se + 50, and humming start se - 20
 
     def swallow_info(self):
         swallowing_start = int(self.df_clip["SS"].iloc[0])
         swallowing_end = int(self.df_clip["SE"].iloc[0])
-        ss = swallowing_start - self.zero_frame + 1
-        se = swallowing_end - self.zero_frame + 1
-        # most of final frames are se + 50
+        ss = swallowing_start - self.zero_frame
+        se = swallowing_end - self.zero_frame
         return ss, se
 
     def hyoid_info(self):
         hb_onset = int(self.df_clip["HBR"].iloc[0])
         hb_maximum = int(self.df_clip["HBM"].iloc[0])
         hb_offset = int(self.df_clip["HBOS"].iloc[0])
-        hb_on = hb_onset - self.zero_frame + 1
-        hb_m = hb_maximum - self.zero_frame + 1
-        hb_off = hb_offset - self.zero_frame + 1
+        hb_on = hb_onset - self.zero_frame 
+        hb_m = hb_maximum - self.zero_frame 
+        hb_off = hb_offset - self.zero_frame 
         return hb_on, hb_m, hb_off
 
     def gh_info(self):
         gh_onset = int(self.df_clip["GHR"].iloc[0])
         gh_maximum = int(self.df_clip["GHM"].iloc[0])
         gh_offset = int(self.df_clip["GHRE"].iloc[0])
-        gh_on = gh_onset - self.zero_frame + 1
-        gh_m = gh_maximum - self.zero_frame + 1
-        gh_off = gh_offset - self.zero_frame + 1
+        gh_on = gh_onset - self.zero_frame 
+        gh_m = gh_maximum - self.zero_frame 
+        gh_off = gh_offset - self.zero_frame 
         return gh_on, gh_m, gh_off
 
 

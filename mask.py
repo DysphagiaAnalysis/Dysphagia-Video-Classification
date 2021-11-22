ceb497f72df64cbfd66f7f13a34c0a32d2c8b75b
Suri Feng <0924suri@gmail.com>
2021-11-22 08:24:26 -0800

    Mask Anno data list completed

--- mask.py
+++ mask.py
@@ -2,6 +2,7 @@ from re import L
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import clip
 import read_excel
 import glob
 import os
@@ -12,30 +13,30 @@ class area:
     def __init__(self, df_excel, clip_name):
         self.c = read_excel.clip_info(df_excel, clip_name)
         # "data/task_a01_5ml-l0-5-2021_06_04_08_51_29-segmentation mask 1.1/SegmentationObject/"
-        data_dir = os.path.join(os.path.abspath(os.getcwd()), "data", "*")
+        data_dir = os.path.join(os.path.abspath(os.getcwd()), "E_data", "*")
         data_file_names = glob.glob(data_dir)
+        self.clip_name = clip_name
         for i in range(len(data_file_names)):
-            if clip_name in data_file_names[i]:
+            if self.clip_name in data_file_names[i]:
                 self.mask_folder_path = os.path.join(
                     data_file_names[i], "SegmentationObject", "*"
                 )
                 break
-
+        self.mask_dir = glob.glob(self.mask_folder_path)  # nonono
+        self.mask_dir.sort()
         self.start = self.c.zero_frame
+        self.start_index = self.start - int(self.mask_dir[0].split(".")[-2][-3:])
         self.end = self.c.last_frame
         self.T = self.end - self.start + 1
-        self.mask_frame_path = np.zeros(self.T)
         self.masks = self.get_masks()  # T*W*H
         self.all_frame_area_in_cm2 = self.get_all_frame_area()  # T*1
 
     # DONE
     def get_masks(self):
-        mask_dir = glob.glob(self.mask_folder_path)
-        mask_dir.sort()
-        W, H = Image.open(mask_dir[0]).size
+        W, H = Image.open(self.mask_dir[0]).size
         masks_gray = np.zeros((self.T, W, H))
         for i in range(self.T):
-            image = Image.open(mask_dir[self.start + i])
+            image = Image.open(self.mask_dir[self.start_index + i])  # need to change
             data = np.transpose(np.asarray(image), (1, 0, 2))
             data_gray = 1 / 3 * (data[:, :, 0] + data[:, :, 1] + data[:, :, 2])
             masks_gray[i, :, :] = data_gray
@@ -62,36 +63,43 @@ class area:
         if flag == "max":
             return (
                 np.max(self.all_frame_area_in_cm2),
-                np.argmax(self.all_frame_area_in_cm2) + self.start
+                np.argmax(self.all_frame_area_in_cm2) + self.start,
             )
         if flag == "min":
             return (
                 np.min(self.all_frame_area_in_cm2),
-                np.argmin(self.all_frame_area_in_cm2) + self.start
+                np.argmin(self.all_frame_area_in_cm2) + self.start,
             )
 
 
 if __name__ == "__main__":
-    excel_name = "A.xlsx"
+    excel_name = "E.xlsx"
     r_e = read_excel.r_excel(excel_name)
 
-    clip_name = "a01_5ml-l0-5"
-    r_c = read_excel.clip_info(r_e.df_excel, clip_name)
+    clip_names = []
+    with open("GH_E_list.txt") as file:
+        clip_names = file.readlines()
+        clip_names = [clip.rstrip() for clip in clip_names]
 
-    a = area(r_e.df_excel, clip_name)
-    ghr, ghm = (
-        int(r_c.df_clip["GHR"].iloc[0]),
-        int(r_c.df_clip["GHM"].iloc[0]),
-    )
-    ghr_area = a.frame_area(ghr)
-    ghm_area = a.frame_area(ghm)
-    anno_percentage = a.percentage(ghm, ghr)
-    gh_maximum_area_cal, gh_maximum_cal = a.find_min_or_max_extreme_area("max")
-    gh_minimum_area_cal, gh_minimum_cal = a.find_min_or_max_extreme_area("min")
-    cal_percentage = a.percentage(gh_minimum_cal, gh_maximum_cal)
-    print("area: anno vs. cal")
-    print(ghr_area, ghm_area, anno_percentage)
-    print(gh_maximum_area_cal, gh_minimum_area_cal, cal_percentage)
-    print("frame number: anno vs. cal")
-    print(ghr, ghm, gh_maximum_cal, gh_minimum_cal)
+    for i in range(len(clip_names)):
+
+        r_c = read_excel.clip_info(r_e.df_excel, clip_names[i])
+
+        a = area(r_e.df_excel, clip_names[i])
+        ghr, ghm = (
+            int(r_c.df_clip["GHR"].iloc[0]),
+            int(r_c.df_clip["GHM"].iloc[0]),
+        )
+        ghr_area = a.frame_area(ghr)
+        ghm_area = a.frame_area(ghm)
+        anno_percentage = a.percentage(ghm, ghr)
+        gh_maximum_area_cal, gh_maximum_cal = a.find_min_or_max_extreme_area("max")
+        gh_minimum_area_cal, gh_minimum_cal = a.find_min_or_max_extreme_area("min")
+        cal_percentage = a.percentage(gh_minimum_cal, gh_maximum_cal)
+        print(clip_names[i])
+        print("area: anno vs. cal")
+        print(ghr_area, ghm_area, anno_percentage)
+        print(gh_maximum_area_cal, gh_minimum_area_cal, cal_percentage)
+        print("frame number: anno vs. cal")
+        print(ghr, ghm, gh_maximum_cal, gh_minimum_cal)
 
--- read_excel.py
+++ read_excel.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
+
 class r_excel:
     def __init__(self, excel_name):
-        self.excel_file = "data/" + excel_name
+        self.excel_file = "E_data/" + excel_name
         self.df_excel = self.excel2df()
 
     def excel2df(self):
@@ -16,7 +17,7 @@ class clip_info:
     def __init__(self, df_excel, clip_name):
         self.clip_file = clip_name
         self.df_clip = df_excel[df_excel["Video Name"].str.lower() == clip_name]
-        self.zero_frame = int(self.df_clip["SS"].iloc[0]) 
+        self.zero_frame = int(self.df_clip["SS"].iloc[0])
         self.last_frame = int(self.df_clip["SE"].iloc[0])
         # most of final frames are se + 50, and humming start se - 20
 
@@ -31,18 +32,18 @@ class clip_info:
         hb_onset = int(self.df_clip["HBR"].iloc[0])
         hb_maximum = int(self.df_clip["HBM"].iloc[0])
         hb_offset = int(self.df_clip["HBOS"].iloc[0])
-        hb_on = hb_onset - self.zero_frame 
-        hb_m = hb_maximum - self.zero_frame 
-        hb_off = hb_offset - self.zero_frame 
+        hb_on = hb_onset - self.zero_frame
+        hb_m = hb_maximum - self.zero_frame
+        hb_off = hb_offset - self.zero_frame
         return hb_on, hb_m, hb_off
 
     def gh_info(self):
         gh_onset = int(self.df_clip["GHR"].iloc[0])
         gh_maximum = int(self.df_clip["GHM"].iloc[0])
         gh_offset = int(self.df_clip["GHRE"].iloc[0])
-        gh_on = gh_onset - self.zero_frame 
-        gh_m = gh_maximum - self.zero_frame 
-        gh_off = gh_offset - self.zero_frame 
+        gh_on = gh_onset - self.zero_frame
+        gh_m = gh_maximum - self.zero_frame
+        gh_off = gh_offset - self.zero_frame
         return gh_on, gh_m, gh_off
 
 

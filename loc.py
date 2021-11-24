7056b2d22fe4560ede6e9cabd3e53fb4a4573af0
Suri Feng <0924suri@gmail.com>
2021-11-24 01:06:49 -0800

    loc measurements partly done

--- loc.py
+++ loc.py
@@ -1,20 +1,104 @@
+import os
+import glob
+import util_read_excel
+import pandas as pd
+import math
+
+
+class loc:
+    def __init__(self, df_excel, clip_name):
+        self.c = util_read_excel.clip_info(df_excel, clip_name)
+        self.clip_name = clip_name
+
+        data_dir = os.path.join(os.path.abspath(os.getcwd()), "HB_Anno", "*")
+        data_file_names = glob.glob(data_dir)
+        for loc_file_dir in data_file_names:
+            if self.clip_name in loc_file_dir:
+                self.loc_folder_path = loc_file_dir
+                break
+
+        self.df_hb = self.get_HB()
+        self.df_mandible = self.get_Mandible()
+
+        self.hbr, self.hbm = (
+            int(self.c.df_clip["HBR"].iloc[0]),
+            int(self.c.df_clip["HBM"].iloc[0]),
+        )
+
+    # DONE
+    def get_HB(self):
+        HB_CSV = os.path.join(self.loc_folder_path, "HB.csv")
+        return pd.read_csv(HB_CSV, header=None)
+
+    # DONE
+    def get_Mandible(self):
+        all_files = os.path.join(self.loc_folder_path, "*")
+        names_for_check = glob.glob(all_files)
+        if "M.csv" not in names_for_check:
+            return None
+        else:
+            M_CSV = os.path.join(self.loc_folder_path, "M.csv")
+            return pd.read_csv(M_CSV, header=None)
+
+    # DONE
+    # 500 pixels -> 10 cm; 50 pixels-> 1cm
+    def cal_max_displacement(self):
+        onset_x, onset_y = self.df_hb.iloc[self.hbr]
+        maximum_x, maximum_y = self.df_hb.iloc[self.hbm]
+        return math.sqrt((onset_x - maximum_x) ** 2 + (onset_y - maximum_y) ** 2) / 50
+
+    # DONE
+    def cal_relative_loc(self, timestamp="hbr"):
+        if not self.df_mandible:
+            return None
+        if timestamp == "hbr":
+            x, y = self.df_hb.iloc[self.hbr]
+            ref_x, ref_y = self.df_mandible.iloc[self.hbr]
+        elif timestamp == "hbm":
+            x, y = self.df_hb.iloc[self.hbm]
+            ref_x, ref_y = self.df_mandible.iloc[self.hbm]
+        return math.sqrt((x - ref_x) ** 2 + (y - ref_y) ** 2) / 50
+
+    # DONE
+    def cal_delta_percentage(self, axis="x"):  # in x or y axis
+        if not self.df_mandible:
+            return None
+        x_hbr, y_hbr = self.df_hb.iloc[self.hbr]
+        x_hbm, y_hbm = self.df_hb.iloc[self.hbm]
+        x_hbr_ref, y_hbr_ref = self.df_mandible.iloc[self.hbr]
+        x_hbm_ref, y_hbm_ref = self.df_mandible.iloc[self.hbm]
+        if axis == "x":
+            hbr_relative, hbm_relative = x_hbr - x_hbr_ref, x_hbm - x_hbm_ref
+        elif axis == "y":
+            hbr_relative, hbm_relative = y_hbr - y_hbr_ref, y_hbm - y_hbm_ref
+        return (hbr_relative - hbm_relative) / hbr_relative
+
+    # DONE
+    # theta(HBM - M) - theta(M - HBR)
+    def cal_angle_in_degree(self):
+        if not self.df_mandible:
+            return None
+        x_hbr, y_hbr = self.df_hb.iloc[self.hbr]
+        x_hbm, y_hbm = self.df_hb.iloc[self.hbm]
+        x_hbr_ref, y_hbr_ref = self.df_mandible.iloc[self.hbr]
+        x_hbm_ref, y_hbm_ref = self.df_mandible.iloc[self.hbm]
+        theta1 = math.atan((y_hbm - y_hbm_ref) / (x_hbm - x_hbm_ref)) * 180 / math.pi
+        theta2 = math.atan((y_hbr - y_hbr_ref) / (x_hbr - x_hbr_ref)) * 180 / math.pi
+        return theta1 - theta2
+
+
 if __name__ == "__main__":
 
-    import os
-    import glob
-    import read_excel
-    import pandas as pd
-    import math
-
-    excel_name = "Swallowing Coordination Data_raw (for trimming).xlsx"
-    r = read_excel.r_excel(excel_name)
+    excel_name = "D.xlsx"
+    r = util_read_excel.r_excel(excel_name)
     df = r.df_excel
 
-    folder = "anno_csv"
-    file_dir = os.path.join(os.path.abspath(os.getcwd()), folder, "*.csv")
-    names = glob.glob(file_dir)
-    names = [name.split("/")[-1].split(".")[0] for name in names]
-    print(names)
+    clip_names = []
+    data_list = "GH_E_list.txt"
+    with open(data_list) as file:
+        clip_names = file.readlines()
+        clip_names = [clip.rstrip() for clip in clip_names]
+
     clip = []
     onset2max = []
     max2offset = []
